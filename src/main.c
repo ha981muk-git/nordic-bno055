af30b9e337ecdb33b6795ad1d2e0dbffd96e9f67
SA0717 <sevimli.ati@gmail.com>
2023-05-22 14:41:39 +0200

    removed unused variables

--- src/main.c
+++ src/main.c
@@ -18,13 +18,12 @@ static const struct gpio_dt_spec led3_spec = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 #define READ_SENSOR_INTERVALL 50
 
 /********************************************************/
-/**\name    I2C ADDRESS DEFINITION FOR BNO055           */
+/*I2C ADDRESS*/
 /********************************************************/
 #define BNO055_ADDRESS_A (0x28)
-#define BNO055_ADDRESS_B (0x29)
 
 /***************************************************/
-/**\name    REGISTER ADDRESS DEFINITION  */
+/*REGISTER ADDRESS*/
 /***************************************************/
 /* Page id register definition*/
 
@@ -32,12 +31,6 @@ static const struct gpio_dt_spec led3_spec = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 
 /* PAGE0 REGISTER DEFINITION START*/
 #define BNO055_CHIP_ID_ADDR (0x00)
-#define BNO055_ACCEL_REV_ID_ADDR (0x01)
-#define BNO055_MAG_REV_ID_ADDR (0x02)
-#define BNO055_GYRO_REV_ID_ADDR (0x03)
-#define BNO055_SW_REV_ID_LSB_ADDR (0x04)
-#define BNO055_SW_REV_ID_MSB_ADDR (0x05)
-#define BNO055_BL_REV_ID_ADDR (0X06)
 
 /* Accel data register*/
 #define BNO055_ACCEL_DATA_X_LSB_ADDR (0X08)
@@ -75,53 +68,18 @@ static const struct gpio_dt_spec led3_spec = GPIO_DT_SPEC_GET(LED3_NODE, gpios);
 #define BNO055_QUATERNION_DATA_Z_LSB_ADDR (0X26)
 #define BNO055_QUATERNION_DATA_Z_MSB_ADDR (0X27)
 
-/* Linear acceleration data registers*/
-#define BNO055_LINEAR_ACCEL_DATA_X_LSB_ADDR (0X28)
-#define BNO055_LINEAR_ACCEL_DATA_X_MSB_ADDR (0X29)
-#define BNO055_LINEAR_ACCEL_DATA_Y_LSB_ADDR (0X2A)
-#define BNO055_LINEAR_ACCEL_DATA_Y_MSB_ADDR (0X2B)
-#define BNO055_LINEAR_ACCEL_DATA_Z_LSB_ADDR (0X2C)
-#define BNO055_LINEAR_ACCEL_DATA_Z_MSB_ADDR (0X2D)
 
 /* Status registers*/
 #define BNO055_CALIB_STAT_ADDR (0X35)
-#define BNO055_SELFTEST_RESULT_ADDR (0X36)
-#define BNO055_INTR_STAT_ADDR (0X37)
-#define BNO055_SYS_CLK_STAT_ADDR (0X38)
-#define BNO055_SYS_STAT_ADDR (0X39)
-#define BNO055_SYS_ERR_ADDR (0X3A)
-
-/* Unit selection register*/
-#define BNO055_UNIT_SEL_ADDR (0X3B)
-#define BNO055_DATA_SELECT_ADDR (0X3C)
 
 /* Mode registers*/
 #define BNO055_OPR_MODE_ADDR (0X3D)
-#define BNO055_PWR_MODE_ADDR (0X3E)
 
 #define BNO055_SYS_TRIGGER_ADDR (0X3F)
-#define BNO055_TEMP_SOURCE_ADDR (0X40)
-/* PAGE0 REGISTERS DEFINITION END*/
 
-/* PAGE1 REGISTERS DEFINITION START*/
-/* Configuration registers*/
-#define BNO055_ACCEL_CONFIG_ADDR (0X08)
-#define BNO055_MAG_CONFIG_ADDR (0X09)
-#define BNO055_GYRO_CONFIG_ADDR (0X0A)
-#define BNO055_GYRO_MODE_CONFIG_ADDR (0X0B)
-#define BNO055_ACCEL_SLEEP_CONFIG_ADDR (0X0C)
-#define BNO055_GYRO_SLEEP_CONFIG_ADDR (0X0D)
-#define BNO055_MAG_SLEEP_CONFIG_ADDR (0x0E)
-
-/***************************************************/
-/**\name    Operation Modes  */
-/***************************************************/
 // Operation Modes
 #define OPERATION_MODE_CONFIG (0x00)
-#define OPERATION_MODE_ACCGYRO (0x05)
-
 #define OPERATION_MODE_IMU (0x08)
-#define OPERATION_MODE_NDOF (0x0C)
 
 static const struct device *i2c_dev = DEVICE_DT_GET(I2C_NODE);
 
@@ -133,7 +91,6 @@ static int32_t sleep_msec = 0;
 struct
 {
 	uint8_t device_id;
-	uint8_t mag_cali;
 	uint8_t accel_cali;
 	uint8_t gyro_cali;
 	uint8_t system_cali;
@@ -153,7 +110,6 @@ struct
 	bool isCalibrated;
 
 } bno055;
-
 
 static const struct smf_state states[];
 
@@ -253,7 +209,7 @@ static void set_op_mode_to_imu()
 	else
 	{
 		printk("Operation Mode is set to: 0x%02X \n", read_i2c_buffer[0]);
-		sleep_msec = 10;
+		sleep_msec = 80;
 		smf_set_state(SMF_CTX(&s_obj), &states[READDATA]);
 	}
 }
@@ -390,7 +346,7 @@ static const struct smf_state states[] = {
 	[SETOPMODETOIMU] = SMF_CREATE_STATE(NULL, set_op_mode_to_imu, NULL),
 	[READDATA] = SMF_CREATE_STATE(NULL, read_data, NULL),
 	[READCALIBRATION] = SMF_CREATE_STATE(NULL, read_calibration_data, NULL)};
-// to-do User soll die erfassung der Daten stoppen können.
+
 void main(void)
 {
 	if (!device_is_ready(i2c_dev))
